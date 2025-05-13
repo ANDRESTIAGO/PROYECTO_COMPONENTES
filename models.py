0453e826f4398ac343da10546641a00629c3b051
ANDRESTIAGO <drestiagomorales@gmail.com>
2025-05-13 11:56:06 -0500

    Se arreglo models

--- models.py
+++ models.py
@@ -5,28 +5,44 @@ from typing import Optional
 class Distribuidores(BaseModel):
     nombre: str = Field(..., min_length=2, max_length=50)
     direccion: str = Field(..., min_length=2, max_length=50)
+    class Config:
+        orm_mode = True
+        from_attributes = True
     
 class Componente(BaseModel):
     nombre: str = Field(..., min_length=2, max_length=50)
     tipo: str = Field(..., min_length=2, max_length=30)
     marca: str = Field(..., min_length=2, max_length=30)
     modelo: str = Field(..., min_length=2, max_length=30)
+    class Config:
+        orm_mode = True
+        from_attributes = True
 
 class ComponenteConId(Componente):
     id: int
+    class Config:
+        orm_mode = True
+        from_attributes = True
 
 class DistriConId(Distribuidores):
     id: int
+    class Config:
+        orm_mode = True
+        from_attributes = True
 
 class ComponenteActualizado(BaseModel):
     nombre: Optional[str] = Field(None, min_length=2, max_length=50)
     tipo: Optional[str] = Field(None, min_length=2, max_length=30)
     marca: Optional[str] = Field(None, min_length=2, max_length=30)
     modelo: Optional[str] = Field(None, min_length=2, max_length=30)
+    class Config:
+        orm_mode = True
+        from_attributes = True
 
 class DistriActualizado(BaseModel):
     nombre: Optional[str] = Field(None, min_length=2, max_length=50)
     direccion: Optional[str] = Field(None, min_length=2, max_length=30)
-
     class Config:
+        orm_mode = True
         from_attributes = True
+
