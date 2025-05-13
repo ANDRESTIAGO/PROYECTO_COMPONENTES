73e4750382ab36c8323c778fc2eed06750c43e24
ANDRESTIAGO <drestiagomorales@gmail.com>
2025-05-12 22:42:34 -0500

    Se arreglaron errores

--- models.py
+++ models.py
@@ -3,27 +3,36 @@ from typing import Optional
 from datetime import datetime
 from pydantic import BaseModel
 
-class Distribuidores(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    nombre: str
-    direccion: str
+class Distribuidores(BaseModel):
+    nombre: str = Field(..., min_length=2, max_length=50)
+    direccion: str = Field(..., min_length=2, max_length=50)
 
 class DistribuidorActualizar(SQLModel):
     nombre: Optional[str] = None
     direccion: Optional[str] = None
 
-class Componente(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    nombre: str
-    tipo: str
-    marca: str
-    modelo: str
+class Componente(BaseModel):
+    nombre: str = Field(..., min_length=2, max_length=50)
+    tipo: str = Field(..., min_length=2, max_length=30)
+    marca: str = Field(..., min_length=2, max_length=30)
+    modelo: str = Field(..., min_length=2, max_length=30)
+
 
 class ComponenteActualizado(SQLModel):
     nombre: Optional[str] = None
     tipo: Optional[str] = None
     marca: Optional[str] = None
     modelo: Optional[str] = None
+
+class ComponenteActualizar(BaseModel):
+    nombre: Optional[str] = Field(None, min_length=2, max_length=50)
+    tipo: Optional[str] = Field(None, min_length=2, max_length=30)
+    marca: Optional[str] = Field(None, min_length=2, max_length=30)
+    modelo: Optional[str] = Field(None, min_length=2, max_length=30)
+
+class DistriActualizar(BaseModel):
+    nombre: Optional[str] = Field(None, min_length=2, max_length=50)
+    direccion: Optional[str] = Field(None, min_length=2, max_length=30)
 
 class ComponenteConId(BaseModel):
     id: int
